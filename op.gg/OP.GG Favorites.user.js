4ce422292b1baef4816a82ad1bec2deb21a27672
Adam Koewler <xadamxk@gmail.com>
2022-02-11 18:27:59 -0600

    Update OP.GG Favorites.user.js

--- op.gg/OP.GG Favorites.user.js
+++ op.gg/OP.GG Favorites.user.js
@@ -2,7 +2,7 @@
 // @name        OP.GG Favorites
 // @author      Adam Koewler
 // @namespace   https://github.com/xadamxk/Userscripts
-// @version     1.0.1
+// @version     1.1.0
 // @description Adds shortcut links to OP.GG's headers of favorited profiles
 // @require     https://code.jquery.com/jquery-3.1.1.js
 // @match       *://www.op.gg/*
@@ -21,20 +21,37 @@
 // @copyright   2022+
 // ==/UserScript==
 // ------------------------------ Changelog -----------------------------
+// v 1.1.0: Update selector logic to get favorites from cookie
 // v 1.0.1: Added update and download urls
 // v 1.0.0: Initial commit
 // ------------------------------ Dev Notes -----------------------------
 // Climb safe!
 // ------------------------------ SETTINGS ------------------------------
 // ------------------------------ SCRIPT ------------------------------
-const favoritesElements = $(".FavoriteSummonerList").find(".Item");
-if (!favoritesElements) return null;
+let favCookieStr = '';
+let subdomain = '';
+try {
+    const cookieStr = document.cookie;
+    const cookies = cookieStr.split(";")
+    favCookieStr = cookies.filter((cookie) => {
+        return cookie.includes("_fav")
+    })[0];
+    subdomain = window.location.hostname.split(".")[0];
+} catch (err) {
+    console.log("failed to parse favorites from cookie");
+    return null;
+}
 
-$(".Header").prepend($("<div>").addClass("PastRank").append($("<ul>").addClass("PastRankList").attr({ "id": "opggFavorites" })))
+const favoritesStr = favCookieStr.replace("_fav=", "");
+const favorites = favoritesStr.split("%24");
+const baseLink = `/summoners/${subdomain}/`;
 
-$(favoritesElements).each((index, favoriteItem) => {
-    const link = $(favoriteItem).find("a.Link:first").attr("href");
-    const name = $(favoriteItem).find(".SummonerName").text();
+
+$("#__next > div:eq(0)").prepend($("<div>").addClass("PastRank").append($("<ul>").addClass("PastRankList").attr({ "id": "opggFavorites" })))
+
+$(favorites).each((index, summonerName) => {
+    const link = baseLink + summonerName;
+    const name = summonerName
     if (link && name) {
         $("#opggFavorites").append($("<li>").addClass("Item tip tpd-delegation-uid-1").append($("<a>").attr("href", link).append($("<b>").text(name))));
     }
