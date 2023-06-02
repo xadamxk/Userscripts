a6563c0db883bec2a1efeca16187ba155c606281
Adam Koewler <xadamxk@gmail.com>
2023-06-02 09:23:21 -0500

    Update GitHub-Favorites.user.js

--- github/GitHub-Favorites.user.js
+++ github/GitHub-Favorites.user.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name        GitHub Favorites
 // @author      Adam Koewler
-// @namespace   https://github.com/xadamxk/HF-Scripts
-// @version     1.0.1
+// @namespace   https://github.com/xadamxk/Userscripts
+// @version     1.0.2
 // @description Adds a dropdown for favorites to the GitHub header.
 // @require     https://code.jquery.com/jquery-3.1.1.js
 // @match       *://github.com/*
 // @copyright   2020+
 // ==/UserScript==
 // ------------------------------ Changelog -----------------------------
+// v 1.0.2: Bugfix for partial page changes
 // v 1.0.1: Bugfix for appending to all nav elements
 // v 1.0.0: Initial commit
 // ------------------------------ Dev Notes -----------------------------
@@ -32,26 +33,38 @@ const favorites = [
     new Favorite("item", "HFX 2.0", "/xadamxk/HFX2.0"),
 ];
 // ------------------------------ SCRIPT ------------------------------
-$("nav:eq(0)").append($("<div>").addClass("Header-item position-relative d-none d-md-flex")
-                .append($("<span>"))
-                .append($("<details>").addClass("details-overlay details-reset")
-                        .append($("<summary>").addClass("Header-link").attr({"role":"button", "data-ga-click":"Header, show menu"})
-                                .append($("<span>").text("Favorites").css({"padding-right":"5px"}))
-                                .append($("<span>").addClass("dropdown-caret")))
-                        .append($("<details-menu>").addClass("dropdown-menu dropdown-menu-sw mt-n2").attr({"role":"menu"}).attr({"id":"favoritesContainer"}))
-                       ));
-// Loop favorites
-favorites.map(favoriteItem => {
-    const favorite = new Favorite(favoriteItem.type, favoriteItem.label, favoriteItem.link)
-    switch(favorite.type) {
-        case "seperator":
-            $("#favoritesContainer").append($("<div>").attr({"role":"none"}).addClass("dropdown-divider"))
-            break;
-        case "header":
-            $("#favoritesContainer").append($("<div>").addClass("dropdown-header").append($("<span>").text(favorite.label)))
-            break;
-        case "item":
-            $("#favoritesContainer").append($("<a>").attr({"role":"menuitem", "href":favorite.link}).addClass("dropdown-item").text(favorite.label))
-            break;
+function appendFavorites() {
+    $("nav:eq(0)").append($("<div>").addClass("Header-item position-relative d-none d-md-flex")
+                          .append($("<span>"))
+                          .append($("<details>").addClass("details-overlay details-reset")
+                                  .append($("<summary>").addClass("Header-link").attr({"role":"button", "data-ga-click":"Header, show menu"})
+                                          .append($("<span>").text("Favorites").css({"padding-right":"5px"}))
+                                          .append($("<span>").addClass("dropdown-caret")))
+                                  .append($("<details-menu>").addClass("dropdown-menu dropdown-menu-sw mt-n2").attr({"role":"menu"}).attr({"id":"favoritesContainer"}))
+                                 ));
+    // Loop favorites
+    favorites.map(favoriteItem => {
+        const favorite = new Favorite(favoriteItem.type, favoriteItem.label, favoriteItem.link)
+        switch(favorite.type) {
+            case "seperator":
+                $("#favoritesContainer").append($("<div>").attr({"role":"none"}).addClass("dropdown-divider"))
+                break;
+            case "header":
+                $("#favoritesContainer").append($("<div>").addClass("dropdown-header").append($("<span>").text(favorite.label)))
+                break;
+            case "item":
+                $("#favoritesContainer").append($("<a>").attr({"role":"menuitem", "href":favorite.link}).addClass("dropdown-item").text(favorite.label))
+                break;
+        }
+    });
+}
+
+appendFavorites();
+
+const interval = setInterval(function () {
+    if ($('#favoritesContainer').length) {
+        clearInterval(interval);
+    } else {
+        appendFavorites();
     }
-});
+}, 1000);
